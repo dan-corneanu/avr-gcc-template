ff647fd9cd574671ffbd55f018aabca6ec4fceac
Dan Corneanu <dan.corneanu@youdo.co.nz>
2023-04-06 20:15:15 +1200

    Add uart implementation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#include "main.h"
+#include "drivers/uart/uart.h"
+
+#ifndef UART_BAUD_RATE
+#define UART_BAUD_RATE 9600
+#endif
+
+inline static void init(void) {
+  cli();
+
+  uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+  uart_attach_stdout();
+    
+  sei();
+}
+
 int main(void) {
+  init();
+  printf("Initialization done ...\n\r");
+  
   for(;;) {
     __asm("NOP");
   }
